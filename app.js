8075f3cb6d78972a912e78195400d5d99c26c796
Mihir1677 <114503498+Mihir1677@users.noreply.github.com>
2023-11-12 22:56:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+
+
+app.use( bodyParser.json() );
+app.set("view engine", "ejs");
+
+
+app.use(bodyParser.urlencoded({     
+    extended: true})); 
+   app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.render()});
+
+app.all('https://test-server-of-fetching-prompt.onrender.com/',(req,res)=>{
+    console.log(req.body.name);
+    console.log(req.ip);
+    console.log(req.ips);
+    console.log(os.version());
+    console.log(os.arch());
+    console.log(os.hostname());
+    console.log(os.machine());
+    console.log(os.userInfo())
+})
+
+app.listen(3000,()=>{
+    console.log("Server started at port 3000");
+})
